a0b507109079feae889ef8bc6200456af4233ba3
Jair montenegro <jairmontenegrof@gmail.com>
2022-04-25 23:12:11 -0300

    main structure and styles

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import logocounter  from './images/logo-counter.png';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='logo-container'>
+      <img className='logo' src={logocounter} alt='logo'/>
+      </div>
+      <div className='principal-container'>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+// How to incluide imgs with import sentence. 
