35f678c43542307db6c65df22f0a98aad2a740de
Jair montenegro <jairmontenegrof@gmail.com>
2022-04-26 00:03:42 -0300

    only commit

--- src/components/button.js
+++ src/components/button.js
@@ -1,9 +1,25 @@
 import React from "react";
 
-function button (){
+function button ({text}){
     return(
-      <button>
-        click
+      <button > 
+        {text}
       </button>
     )
 }
+
+
+// new syntaxis to add code more concise 
+//insted  key props we can use DESTRUCTURING SYNTAX so 
+//if you can see the side where the props were, you can see that () has a pair of {} where inside we can give the properties that we may need
+
+//function button (props){
+ // return(
+  //  <button>
+    //  {text}
+    //</button>
+ //
+//}
+
+    // CLASS BASED IN CONDITION (TERNARY OPERATOR) 
+
