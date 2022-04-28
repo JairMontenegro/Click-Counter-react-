c4a67e815ed12cf92963f5701b0a6548058a0fdf
Jair montenegro <jairmontenegrof@gmail.com>
2022-04-27 23:23:12 -0300

    we've created components of buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import './App.css';
+import ButtonFun from './components/button';
 import logocounter  from './images/logo-counter.png';
 
+
 function App() {
+
+  const setClick = () => {
+      console.log('click')
+  }
+const restartCounter = () => {
+  console.log('restart')
+}
+
+
   return (
     <div className='App'>
       <div className='logo-container'>
       <img className='logo' src={logocounter} alt='logo'/>
       </div>
+
       <div className='principal-container'>
-        
+      
+
+      <ButtonFun
+      text = 'Click'
+      isClickButton = {true}
+      setClick = {setClick}
+      />
+      <ButtonFun
+      text ='Reset'
+      isClickButton = {true}
+      setClick = {restartCounter}/>
+    
       </div>
     </div>
   );
--- src/components/button.js
+++ src/components/button.js
@@ -1,25 +1,21 @@
 import React from "react";
 
-function button ({text}){
+function ButtonFun ({text, isClickButton, setClick }) {
     return(
-      <button > 
+      <button  
+      className={ isClickButton ? 'click-button' : 'restart-button' }
+      onClick = {setClick} >
         {text}
       </button>
     )
 }
 
+export default ButtonFun;
+
 
 // new syntaxis to add code more concise 
 //insted  key props we can use DESTRUCTURING SYNTAX so 
 //if you can see the side where the props were, you can see that () has a pair of {} where inside we can give the properties that we may need
-
-//function button (props){
- // return(
-  //  <button>
-    //  {text}
-    //</button>
- //
-//}
-
-    // CLASS BASED IN CONDITION (TERNARY OPERATOR) 
-
+//also we need to notice that the classname of our component button is a dinamuc class which it have a conditional ternary. remember ? : javascript basic
+// and we add an event listtener to our component as a parameter which  is a function that will allow it to happen when you click the button. 
+// remember that the react comoments HAVE TO START WITH UPPERCASE. 
