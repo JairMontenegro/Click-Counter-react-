0f8eeb1d19fa40dacd35ceca6adccd2f2387ebe8
Jair montenegro <jairmontenegrof@gmail.com>
2022-05-04 16:18:01 -0300

    Updates

---
+++ precache-manifest.72e359aa7da11007184186dcf15f9a29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11acd9eabbad31396a20",
+    "url": "/Click-Counter-react-/static/css/main.3e814e73.chunk.css"
+  },
+  {
+    "revision": "11acd9eabbad31396a20",
+    "url": "/Click-Counter-react-/static/js/main.11acd9ea.chunk.js"
+  },
+  {
+    "revision": "3d978e9a755fb10b1846",
+    "url": "/Click-Counter-react-/static/js/1.3d978e9a.chunk.js"
+  },
+  {
+    "revision": "a2e01fca1dbee8f1b4c0",
+    "url": "/Click-Counter-react-/static/js/runtime~main.a2e01fca.js"
+  },
+  {
+    "revision": "9860f6ac7b927dc01a7e76c10522ca77",
+    "url": "/Click-Counter-react-/static/media/logo-counter.9860f6ac.png"
+  },
+  {
+    "revision": "16fea2139d2a0c1372cffd3182fdd544",
+    "url": "/Click-Counter-react-/index.html"
+  }
+];
